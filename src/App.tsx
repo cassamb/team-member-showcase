be8bd66e530257b42c66b676f8cb02179d039b9c
cassamb <rosecass99@gmail.com>
2026-03-10 18:14:37 -0400

    Initial Commit
    Initialized React project, populated team member data in JSON file, and created Header Component

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+
+function App() {
+
+  return (
+    <>
+      <main className="container max-w-5xl">
+        <Header/>
+      </main>
+    
+    </>
+    
+  )
+}
+
+export default App
